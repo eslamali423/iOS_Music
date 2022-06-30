7e1ed7b4946bf1f73ea2f9aa88e7801ad8687823
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-06-30 18:13:24 +0200

    setup tableview in music viewController

--- Podcast/Podcast/Views/Controllers/Core/MusicViewController.swift
+++ Podcast/Podcast/Views/Controllers/Core/MusicViewController.swift
@@ -9,11 +9,72 @@ import UIKit
 
 class MusicViewController: UIViewController {
 
+    //MARK:- Vars
+    var albumsViewModel = AlbumsViewModel()
+    
+    private let tableView : UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.backgroundColor = .clear
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return table
+    }()
+    
+    
+    
+    //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        getAlbums()
+        configureHeaderView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        
+    }
+    
+    
+    //MARK:- Navbar Configuration
+    
+    
+    
+    //MARK:- Customize Header  for the TableView
+    func configureHeaderView()  {
+        let  headerView = HeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height / 3))
+        tableView.tableHeaderView = headerView
+        
+    }
+    
+    
+    //MARK:- Get All Albums
+    func getAlbums()  {
+        albumsViewModel.getAlbums { (isSuccess) in
+            if isSuccess{
+                print("Get Data Successfully")
+            }
+        }
     }
     
 
+}
+
+//MARK:- extension for TableView delegate and datasource
+extension MusicViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
