291e92630669ca9ae167a1979d96eada48dd990d
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-06-30 20:29:44 +0200

    implement AlbumsDelegate in MusicView Controller and open safariPage

--- Podcast/Podcast/Views/Controllers/Core/MusicViewController.swift
+++ Podcast/Podcast/Views/Controllers/Core/MusicViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import ProgressHUD
+import SafariServices
 
 class MusicViewController: UIViewController {
 
@@ -24,11 +28,15 @@ class MusicViewController: UIViewController {
     //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        configureNavBar()
         view.addSubview(tableView)
+        
+        tableView.isHidden = true
+        
+        showProgress()
         getAlbums()
-        configureHeaderView()
+        
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -43,12 +51,38 @@ class MusicViewController: UIViewController {
     
     
     //MARK:- Navbar Configuration
-    
+    func configureNavBar()  {
+        
+        // set the logo in the center
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+      //  imageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.image = UIImage(systemName: "applelogo", withConfiguration: UIImage.SymbolConfiguration(scale: .large))
+        
+        let contentView = UIView()
+        self.navigationItem.titleView = contentView
+        self.navigationItem.titleView?.addSubview(imageView)
+        imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        
+        
+        // set the left buton to the navBar
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .done, target: self, action: nil)
+        
+        // set the left buton to the navBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.circle"), style: .done, target: self, action: nil)
+        
+        
+        navigationController?.navigationBar.tintColor = .systemPink
+    }
     
     
     //MARK:- Customize Header  for the TableView
-    func configureHeaderView()  {
-        let  headerView = HeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height / 3))
+    func configureHeaderView(albums : BehaviorSubject<[Album]>)  {
+        let  headerView = HeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height / 2.7), albums: albums)
+        headerView.delegate = self
         tableView.tableHeaderView = headerView
         
     }
@@ -56,11 +90,21 @@ class MusicViewController: UIViewController {
     
     //MARK:- Get All Albums
     func getAlbums()  {
-        albumsViewModel.getAlbums { (isSuccess) in
+        albumsViewModel.getAlbums { [weak self ](isSuccess) in
             if isSuccess{
-                print("Get Data Successfully")
+                guard self == self else {return }
+                self?.configureHeaderView(albums: self!.albumsViewModel.albumsBehaviorSubject)
+                self?.tableView.reloadData()
+                self?.tableView.isHidden = false
+                ProgressHUD.dismiss()
             }
         }
+    }
+    
+    private func showProgress(){
+        ProgressHUD.animationType = .circleStrokeSpin
+        ProgressHUD.colorAnimation = .systemPink
+        ProgressHUD.show()
     }
     
 
@@ -74,6 +118,16 @@ extension MusicViewController : UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+    
+    
+}
+//MARK:- extension for Albums delegate
+extension MusicViewController : AlbumsDelegate {
+    func didSelectAlbum(url: URL) {
+        let albumVC =  SFSafariViewController(url: url)
+        albumVC.navigationController?.title = "sdfsdfsdf"
+        present(albumVC, animated: true, completion: nil)
     }
     
     
